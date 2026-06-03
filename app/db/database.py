b10ce67a5b7e9630e949204b953fc8ec30212738
Naresh Mahapatra <codes.xdev@gmail.com>
2026-06-03 23:24:51 +0530

    Database connectivity issue resolved

--- app/db/database.py
+++ app/db/database.py
@@ -2,9 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from app.config import settings
 
+try:
+    engine = create_engine(str(settings.DATABASE_URL) , echo=True)
+    SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+except Exception as e:
+    print(f"error : {e}")
 
-engine = create_engine(settings.DATABASE_URL , echo=True)
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class Base(DeclarativeBase):
     pass
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from app.routers import expenses , budgets
 from app.db.database import Base, engine
+from app.db.deps import SessionDep
+from app.models.budget import Budget
+from app.models.expense import Expense
 
 
 Base.metadata.create_all(bind=engine)
@@ -13,7 +16,7 @@ def root():
 
 @app.get("/reports/monthly")
 def get_monthly_report():
-    pass
+    return {"message" : "May Monthly Report is On the Way !!"}
 
 app.include_router(expenses.router , prefix="/expenses")
 app.include_router(budgets.router , prefix="/budgets")
